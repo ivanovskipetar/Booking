343e5fb34339394f76c5c0d3f60ba256cd2dffa2
Petar Ivanovski <ivanovskipetar4@gmail.com>
2024-07-25 23:22:52 +0200

    LinksDropdown component design

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { TfiUser } from "react-icons/tfi";
 
 function UserIcon() {
-  return <div>UserIcon</div>;
+  return <TfiUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 
 export default UserIcon;
